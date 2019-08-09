e569b5a5fd4c52195b1d2a61c417f52310edc6b5
noelz8 <48070788+noelz8@users.noreply.github.com>
2019-08-09 08:35:05 -0600

    Add files via upload

---
+++ area rect primera programa 9-8-19.py
@@ -0,0 +1,10 @@
+#Primera prgramacion intro II semestre
+#9/8/2019
+#Funcion que calcula el area de un rectangulo
+#E(entrada): base,altura
+#S(salida): area del rectangulo
+#R(restrincciones): base,altura numeros y mayores que 0
+
+def area_rect(base,altura):
+    area==base*altura
+    return area
